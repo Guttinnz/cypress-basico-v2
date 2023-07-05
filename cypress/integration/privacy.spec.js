1418d60cdd77d7974425c05163277671ff8a740b
Gustavo A.R. Bueno <gustavoriedel2202@gmail.com>
2023-07-04 23:45:08 -0300

    :sparkles: add readme and pipeline to the project

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,5 @@
-describe('Política de privacidade', () => {
+// describe('Política de privacidade', () => {
+  Cypress._.times(3, function(){
     it('testa a página da política de privacidade de forma independente', () => {
       cy.visit('./src/privacy.html')
       cy.contains('Talking About Testing').should('be.visible')
